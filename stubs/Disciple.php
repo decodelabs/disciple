fea6e06e0d9c6b7b2c2287f46c8b4c52db585f6a
Tom Wright <tom@inflatablecookie.com>
2021-05-11 11:21:57 +0000

    Added Veneer IDE support stub
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ stubs/Disciple.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This is a stub file for IDE compatibility only.
+ * It should not be included in your projects.
+ */
+namespace DecodeLabs;
+use DecodeLabs\Veneer\Proxy;
+use DecodeLabs\Veneer\ProxyTrait;
+use DecodeLabs\Disciple\Context as Inst;
+class Disciple implements Proxy { use ProxyTrait; 
+const VENEER = 'Disciple';
+const VENEER_TARGET = Inst::class;};
